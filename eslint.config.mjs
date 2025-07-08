932d4b0d53981ffa6e3507820c9a609435f1ed50
ErwanDecoster <wanerd2@gmail.com>
2025-07-08 22:18:23 +0200

    chore: update ESLint configuration to disable consistent type specifier style rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt(
+  {
+    rules: {
+      "import/consistent-type-specifier-style": "off",
+    },
+  }
+);
 // Your custom configs here
