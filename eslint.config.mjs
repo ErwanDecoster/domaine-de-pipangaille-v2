043654a2b242986a80d38ef22a723d3ae6bb89d0
ErwanDecoster <wanerd2@gmail.com>
2025-07-08 22:23:31 +0200

    chore: format ESLint configuration for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  {
-    rules: {
-      "import/consistent-type-specifier-style": "off",
-    },
-  }
-);
+export default withNuxt({
+  rules: {
+    "import/consistent-type-specifier-style": "off",
+  },
+});
 // Your custom configs here
