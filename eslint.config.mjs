37ad9eb53a4b93cf5a17f0c06abe6547b1daa1b0
ErwanDecoster <wanerd2@gmail.com>
2025-07-09 20:22:08 +0200

    chore: add .nvmrc file and enhance ESLint configuration for consistent type imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   rules: {
     "import/consistent-type-specifier-style": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
   },
   files: ["**/*.{ts,tsx,vue}"],
 });
